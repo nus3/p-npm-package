ddcae616c4f6226498c0b0c22185174305d17d3b
yota-hada <hadayota33@gmail.com>
2021-08-25 18:48:22 +0900

    fix(p-component): Add vite-react-jsx

--- packages/p-component/vite.config.ts
+++ packages/p-component/vite.config.ts
@@ -1,13 +1,11 @@
 import path from 'path'
 import { defineConfig } from 'vite'
 import reactRefresh from '@vitejs/plugin-react-refresh'
+import reactJsx from 'vite-react-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh()],
-  esbuild: {
-    jsxInject: `import React from 'react'`,
-  },
+  plugins: [reactRefresh(), reactJsx()],
   build: {
     lib: {
       entry: path.resolve(__dirname, 'src/index.ts'),
